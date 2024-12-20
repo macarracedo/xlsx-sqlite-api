49955f357a39c1ee1cfe13fa84447efc97b72b21
Manuel Alonso Carracedo <manuelalonsocarracedo@gmail.com>
2024-12-20 11:09:24 +0100

    changes module names

--- unicef/datamerge/views.py
+++ unicef/datamerge/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.models import Group, User
 from rest_framework import permissions, viewsets
 
-from tutorial.quickstart.serializers import GroupSerializer, UserSerializer
+from unicef.datamerge.serializers import GroupSerializer, UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
--- unicef/urls.py
+++ unicef/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 
-from tutorial.quickstart import views
+from unicef.datamerge import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
