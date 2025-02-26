c0b7fce7416ea76289bf5fa2336a91b9fe426fa1
Manuel Alonso Carracedo <manuelalonsocarracedo@gmail.com>
2025-02-26 12:14:18 +0100

    update database settings to use PostgreSQL credentials for user and password

--- unicef/settings.py
+++ unicef/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     "default":{
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "unicef",
-        "USER": "unicef",
-        "PASSWORD": "unicef",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
         "HOST": "10.10.10.129",
         "PORT": "5432",
     },
