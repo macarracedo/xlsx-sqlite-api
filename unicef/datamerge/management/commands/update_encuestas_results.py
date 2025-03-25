f37b0b03d8877f828ac70fde3522e44737e581b2
Manuel Alonso Carracedo <manuelalonsocarracedo@gmail.com>
2025-03-25 16:29:02 +0100

    Import update_encuestas_results function inside handle method to avoid circular import

--- unicef/datamerge/management/commands/update_encuestas_results.py
+++ unicef/datamerge/management/commands/update_encuestas_results.py
@@ -1,5 +1,4 @@
 from django.core.management.base import BaseCommand
-from unicef.datamerge.views import update_encuestas_results
 import logging
 
 logging.basicConfig(level=logging.INFO)
@@ -10,5 +9,9 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         logging.info("Starting update_encuestas_results command")
+
+        # Import the function inside the handle method to avoid circular import
+        from unicef.datamerge.views import update_encuestas_results
+
         update_encuestas_results()
         logging.info("Finished update_encuestas_results command")
